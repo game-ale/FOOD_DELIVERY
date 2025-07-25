6bba85009263363b484800d324be49e94f4bfa7b
Gemechu Alemu <alemugemechu44@gmail.com>
2025-07-25 16:53:31 +0300

    Constant color Widgets created

---
+++ food_delivery_app/lib/common/themes/App_Color.dart
@@ -0,0 +1,24 @@
+    import 'package:flutter/material.dart';
+class AppColors {
+  static 
+  final Color textColor = Color(0xFFccc7c5); // Deep Blue
+ // Lighter Blue
+  static 
+  final Color mainColor = Color(0xFF89dad0); // Amber
+  static 
+  final Color iconColor1 = Color(0xFFffd28d); // Light Gray
+  static 
+  final Color iconColor2 = Color(0xFFfcab88); // Dark Gray
+  static 
+  final Color paraColor = Color(0xFF8f837f); // Medium Gray
+  static 
+  final Color  buttonBackgroundColors= Color(0xFFf7f6f4); // Light Border Gray
+  static 
+  final Color signColor = Color(0xFFa9a29f); // Dark Border Gray
+  static 
+  final Color titleColor = Color(0xFF5c524f); 
+  static 
+  final Color mainBlackColor = Color(0xFF332d2b); 
+  static 
+  final Color yellowColor = Color(0xFFffd379); 
+}
